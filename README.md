08b2b8c22fd30ee23bc80798e6f58bbd50c13230
SoulmongerV2 <matchetator@gmail.com>
2023-05-12 22:54:11 +0200

    Create basic layout Log round results to the game-log panel

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-ROCK-PAPER-SCISSOR Project for Odin Project Javascript Fundamentals
+ROCK-PAPER-SCISSOR Project for Odin Project Javascript Fundamentals<br>
+
+
+
+
+Image by <a href="https://pixabay.com/users/openclipart-vectors-30363/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=576668">OpenClipart-Vectors</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=576668">Pixabay</a>
+
+Image by <a href="https://pixabay.com/users/openclipart-vectors-30363/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1297554">OpenClipart-Vectors</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1297554">Pixabay</a>
+
+Image by <a href="https://pixabay.com/users/openclipart-vectors-30363/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=154169">OpenClipart-Vectors</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=154169">Pixabay</a>
+
+Image by <a href="https://pixabay.com/users/openclipart-vectors-30363/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1296041">OpenClipart-Vectors</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1296041">Pixabay</a>
+
+Image by <a href="https://pixabay.com/users/openclipart-vectors-30363/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1293303">OpenClipart-Vectors</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1293303">Pixabay</a>
+
+Image by <a href="https://pixabay.com/users/aroosakhan1214-29418690/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=7736814">Aroo Khan</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=7736814">Pixabay</a>
--- script.js
+++ script.js
@@ -1,80 +1,144 @@
-function playRockPaperScissors(rounds = 5){
+let roundCount;
+let playerScore;
+let compScore;
+let gameLog = document.querySelector('.game-log');
 
-    for(let i = 0; i < rounds; i++){
-        console.log(playRound(getPlayerChoice(), getComputerChoice()));     //plays a round and outputs results
+gameInit();
+
+let choiceButtons = document.querySelectorAll('.player-choice');
+choiceButtons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        playRound(btn.id);
+    })});
+
+function gameInit(){
+    roundCount = 0;
+    playerScore = 0;
+    compScore = 0;
+}
+
+function playRound(playerChoice){
+    /**************************************************
+     *Compares the players' choices and decides a winner. 
+        *Returns an array with a game result on index [0]  and game description on index [1], in reference to player_one
+        *Results:
+        *   player_one won  -  +1
+        *   players tied    -   0
+        *   player_one lost -   1
+    ***************************************************/
+    let compChoice = getComputerChoice();
+    let winner = getWinner();
+    awardPoints(winner);
+    roundCount++;
+    logGame();
+
+    function logGame(){
+        let newLogLine = document.createElement('p');
+        newLogLine.classList.add('game-log-line');
+        newLogLine.textContent = generateLog();
+        gameLog.appendChild(newLogLine);
     }
 
-    function getPlayerChoice(){
-        /**************************************************
-         * Reads and returns player's choice via prompt
-         **************************************************/
-
-        return prompt("Choose Rock, Paper or Scissors.");
+    function generateLog(){
+        return `Player: ${playerChoice}, CPU: ${compChoice}, Winner: ${winner}`;
     }
-    
+
+    function awardPoints(winner){
+        switch(winner){
+            case playerChoice:
+                playerScore++;
+                break;
+            case compChoice:
+                compScore++;
+                break;
+            default:       
+        }
+
+        console.log(`player ${playerScore}, comp ${compScore}`);
+    }
+
     function getComputerChoice(){
         /**************************************************
          * Returns "Rock", "Paper" or "Scissors" randomly
          **************************************************/
-
+    
         const compChoice = Math.floor(Math.random() * 3 + 1);
         switch(compChoice){
             case 1:
-                return "Rock";
+                return "rock";
             case 2:
-                return "Paper";
+                return "paper";
             case 3:
-                return "Scissors";
+                return "scissors";
             default:
         }
     }
 
-    function playRound(player_one, player_two){
-        /**************************************************
-         *Compares the players' choices and decides a winner. 
-         *Returns an array with a game result on index [0]  and game description on index [1], in reference to player_one
-         *Results:
-         *   player_one won  -  +1
-         *   players tied    -   0
-         *   player_one lost -   1
-        ***************************************************/
-
-        player_one = player_one.toLowerCase();
-        player_two = player_two.toLowerCase();
-
-        //console.log(`P1 ${player_one), P2 ${player_two}`); --for testing purposes
-
-        if(player_one === player_two){
+    function getWinner(){
+        if(playerChoice === compChoice){
+            return "Tie";
+        }
+    
+        if(playerChoice === "rock"){
+            if(compChoice === "paper"){
+                return "CPU";
+            }
+            if(compChoice === "scissors"){
+                return "Player";
+            }
+        }
+    
+        if(playerChoice === "paper"){
+            if(compChoice === "scissors"){
+                return "CPU";
+            }
+            if(compChoice === "rock"){
+                return "Player";
+            }
+        }
+    
+        if(playerChoice === "scissors"){
+            if(compChoice === "rock"){
+                return "CPU";
+            }
+            if(compChoice === "paper"){
+                return "Player";
+            }
+        }
+    }
+    /*
+    function getWinner(){
+        if(playerChoice === compChoice){
             return [0,"It's a tie."];
         }
-
-        if(player_one === "rock"){
-            if(player_two === "paper"){
+    
+        if(playerChoice === "rock"){
+            if(compChoice === "paper"){
                 return [-1,"You lost. Paper beats Rock."];
             }
-            if(player_two === "scissors"){
+            if(compChoice === "scissors"){
                 return [1,"You won. Rock beats Scissors"];
             }
         }
-
-        if(player_one === "paper"){
-            if(player_two === "scissors"){
+    
+        if(playerChoice === "paper"){
+            if(compChoice === "scissors"){
                 return [-1, "You lost. Scissors beat Paper"];
             }
-            if(player_two === "rock"){
+            if(compChoice === "rock"){
                 return [1, "You won. Paper beats Rock"];
             }
         }
-
-        if(player_one === "scissors"){
-            if(player_two === "rock"){
+    
+        if(playerChoice === "scissors"){
+            if(compChoice === "rock"){
                 return [-1,"You lost. Rock beats Scissors"];
             }
-            if(player_two === "paper"){
+            if(compChoice === "paper"){
                 return [1, "You won. Scissors beat Paper"];
             }
         }
-        
     }
-
+    */
 }
+
